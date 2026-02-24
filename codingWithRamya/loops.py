e08f7bd55524c6c7d709cf53ecc6aac485f4b7e0
Ramya C N <ramyacn1645@gmaul.com>
2026-02-24 15:27:24 +0530

    added loop1

--- codingWithRamya/loops.py
+++
@@ -1,6 +0,0 @@
-age = int(input("enter your age:"))
-if age < 18:
-    print("you are minor")
-else:
-    print("you are adult\n you can vote")
-
---
+++ codingWithRamya/loops1.py
@@ -0,0 +1,7 @@
+list = [121, "Ramya", "Krishna", 12.5]
+i = 0
+while(len(list) >i):
+    print(list[i])
+    i +=1
+        
+    
