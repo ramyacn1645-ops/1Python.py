02aa17f8974e03887d1008ad3c2904cbad80e53c
Ramya C N <ramyacn1645@gmail.com>
2026-03-02 23:43:19 +0530

    updated

--- dataStruct.py/demo.py
+++ dataStruct.py/demo.py
@@ -1,13 +1,8 @@
-side_a =int(input("enter value of side_a:"))
-side_b =int(input("enter value of side_b:"))
-side_c =int(input("enter value of side_c:"))
+# Import a powerful tool called "pandas" that you'll use to work with and organize data easily
+import pandas as pd
 
-if(side_a==side_b and side_b==side_c):
-    print("it is an equilateral triangle")
-elif(side_a==side_b or side_b==side_c or side_a==side_c):
-    print("it is an isosceles triangle")
-else:
-    print("it is a scalene triangle")
+# Load the book catalog from the CSV file
+book_catalog_df = pd.read_csv('book_catalog_10.csv')
 
 
 
