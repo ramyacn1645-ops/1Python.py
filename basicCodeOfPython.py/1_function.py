c57a9d8859b9d7be7120400ce981eb3ca949e455
Ramya C N <ramyacn1645@gmaul.com>
2026-03-02 19:30:51 +0530

    new file added

--- basicCodeOfPython.py/1_function.py
+++ basicCodeOfPython.py/1_function.py
@@ -1,20 +1,12 @@
 def avg():
-<<<<<<< HEAD
-
-    a = int(input("enter the first number"))
-    b = int(input("enter the second number"))
-    c = int(input("enter the third number"))
-    average = (a+b+c)/3
-    print(f"the average of {a},{b},{c} is {average}")
-    avg(5)
-=======
     a=3
     b=3
     c=4
     averge = (a+b+c)/3
     return averge
 
+
 avg()
+print(avg())
     
     
->>>>>>> 91d4b508612e1a055e19d79f0bb5cef2e540eb8f
---
+++ dataStruct.py/demo.py
@@ -0,0 +1,13 @@
+side_a =int(input("enter value of side_a:"))
+side_b =int(input("enter value of side_b:"))
+side_c =int(input("enter value of side_c:"))
+
+if(side_a==side_b and side_b==side_c):
+    print("it is an equilateral triangle")
+elif(side_a==side_b or side_b==side_c or side_a==side_c):
+    print("it is an isosceles triangle")
+else:
+    print("it is a scalene triangle")
+
+
+
