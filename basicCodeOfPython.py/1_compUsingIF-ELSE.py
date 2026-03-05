61800b54c6e9cb6d76828e274775e53d1f8f0530
Ramya C N <ramyacn1645@gmail.com>
2026-03-05 23:57:35 +0530

    Fix print statement for comparing three numbers

--- basicCodeOfPython.py/1_compUsingIF-ELSE.py
+++ basicCodeOfPython.py/1_compUsingIF-ELSE.py
@@ -9,4 +9,4 @@ def compare_three_nums(a,b,c):
 a = int(input("enter the value of a:"))
 b = int(input("enter the value of b:"))
 c = int(input("enter the value of c:"))
-print("the greatest of three number is ",compare_three_nums(a,b,c
+print("the greatest of three number is ",compare_three_nums(a,b,c)
