94f71b6e13d5f149c4f24bf8d135536cf0ccade5
Ramya C N <ramyacn1645@gmail.com>
2026-03-25 22:33:35 +0530

    Update List.py

--- basicCodeOfPython.py/List.py
+++ basicCodeOfPython.py/List.py
@@ -1,1 +1,26 @@
+num = int(input("how many elements are in the list?\n"))
+number =[]
 
+for i in range(num):
+    print("Enter the number: ")
+
+for i in range(num):
+    number.append(int(input("Enter the number:")))
+
+
+print("The number in the list:\n")
+print(number)
+sum=0
+
+for i in range(num):
+    sum = sum+ number[i]
+    sum1=0
+  
+print("sum is: ", sum)
+mean = sum/num
+print("mean is: ",mean)
+
+for i in range(num):
+      sum1 = sum1 + 
+  
+     
