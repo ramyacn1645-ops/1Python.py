eb6d52091a0a2b6c4fd14108ef7ce29eb98b859e
Ramya C N <ramyacn1645@gmail.com>
2026-03-26 23:40:13 +0530

    Update list operations.py

--- basicCodeOfPython.py/list operations.py
+++ basicCodeOfPython.py/list operations.py
@@ -3,3 +3,14 @@ list = [1,2,34,abc,4]
 list.append(55)
 print(list)
 
+# removing a element from a list
+list =[2,3,33,44]
+list.remove() # it removes the last element
+print(list)
+# removing element from a list using poo operator 
+list=[1,2,3,4,5]
+list.pop() # removes the last element 
+print(list)
+list.pop(3) # removes the element in the index 3
+print(list)
+
