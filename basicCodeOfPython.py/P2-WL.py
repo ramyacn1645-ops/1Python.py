9c80e2fc6b335cbad085e301a7856ce7a1f33428
Ramya C N <ramyacn1645@gmail.com>
2026-03-27 20:20:02 +0530

    updated

---
+++ basicCodeOfPython.py/P2-WL.py
@@ -0,0 +1,7 @@
+# to obtain product of n natural numbers
+n = int(input("Enter a number"))
+product =1
+for i in range(1, n+1):
+    product = product*1
+
+print(f"The product of {n} natural numbers is: {product}")
